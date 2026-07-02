0c6e250f7d19c96457149094db22930d2fb663cb
sanjayvarma56 <dssvarma56@gmail.com>
2026-07-02 07:59:37 +0530

    Flask_session

---
+++ sessionex.py
@@ -0,0 +1,24 @@
+from flask import Flask,session,redirect,url_for,render_template,request
+app = Flask(__name__)
+app.secret_key = 'any random string'
+@app.route('/')
+def index():
+    if 'username' in session:
+        username = session['username']
+        return 'Logged in as ' + username+ '<br>' + \
+        "<b><a href = '/Logout '>click here to logout </a></b>"
+    return "You are not logged in <br><a href = '/login'></b>" + \
+            "click here to log in </b></a>"
+@app.route('/logn', methods = ['GET','POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return(redirect(url_for('index')))
+    return render_template('session.html')
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
